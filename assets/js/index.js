95b0c145b9b4bbe40fb707c1b9598c605f2244a1
KismiOfficial <kismisolutions@gmail.com>
2025-02-04 23:05:00 -0800

    Initial Update

---
+++ assets/js/index.js
@@ -0,0 +1,40 @@
+import 'regenerator-runtime/runtime';
+import axios from 'axios';
+
+(function() {
+    console.log('Test!');
+    
+    const testbutton = document.getElementById('test-btn');
+    
+    testbutton.addEventListener('click', async _ => {
+        console.log('Inside!');
+        
+        try {
+            axios.post(
+              'https://api.hubapi.com/crm/v3/objects/contacts',
+              {
+                headers: {
+                  Authorization: `Bearer pat-na1-be681d2e-e7d8-4cb9-8f5c-47783a93c1ba`,
+                  'Content-Type': 'application/json',
+                },
+                body: {
+                    "properties": {
+                        "email": "testuseremail@gmail.com",
+                        "firstname": "John",
+                        "lastname": "Smith"
+                    }
+                }
+              },
+              (err, data) => {
+                // Handle the API response
+              }
+            );
+            console.log('After');
+            return 'End';
+            } catch (e) {
+                console.log(e);
+                return e;
+            }
+    });
+    
+})();
