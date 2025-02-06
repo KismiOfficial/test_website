fa7d1eeba33518177b964f5ffcc267a403bbecc1
KismiOfficial <kismisolutions@gmail.com>
2025-02-06 14:39:45 -0800

    Adding fields to the form

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,6 +8,12 @@ import axios from 'axios';
     const postbutton = document.getElementById('post-btn');
     postbutton.addEventListener('click', async _ => {
       console.log('Inside Post');
+      for (const el of document.getElementById('form').querySelectorAll("[required]")) {
+        if (!el.reportValidity()) {
+          messageField.textContent = "Please fill out all the required fields";
+          return;
+        }
+      }
       
       try{
       
@@ -42,41 +48,6 @@ import axios from 'axios';
       }
     });
         
-    const testbutton = document.getElementById('test-btn');
-    testbutton.addEventListener('click', async _ => {
-      console.log('Inside Post');
-      
-      try{
-      
-        const data = {
-          email: 'email@example.com',
-          firstname: 'John',
-          lastname: 'Doe',
-          phone: '123-456-7890'
-        };
-      
-        const response = await fetch('https://test-website-seven-bice.vercel.app/api/sendToHubspot', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Credentials': 'true',
-            'Access-Control-Allow-Methods': '*',
-            'Access-Control-Allow-Headers': '*',
-          },
-          body: JSON.stringify(data)
-        });
-        
-        const responseData = await response.json();
-        console.log("Response Data: ", responseData);
-        if(responseData.message === 'Conflict'){
-          messageField.textContent = 'That user is already in the member database';
-        }
-      } catch (error){
-        console.error('Error submitting form:', error);
-        messageField.textContent = 'An error occurred while submitting the form.';
-      }
-    });
     
     const getbutton = document.getElementById('get-btn');
     getbutton.addEventListener('click', async _ => {
@@ -100,27 +71,5 @@ import axios from 'axios';
       }
         
     });
-      
-      //console.log('Inside Get');
-      //try{
-      //const response = await axios.get(
-      //  'https://test-website-seven-bice.vercel.app/api/sendToHubspot',
-      //  {
-      //    headers: {
-      //      'Content-Type': 'application/json',
-      //      'Access-Control-Allow-Origin': '*',
-      //      'Access-Control-Allow-Credentials': 'true',
-      //      'Access-Control-Allow-Methods': '*',
-      //      'Access-Control-Allow-Headers': '*',
-      //    },
-      //  },
-      //);
-      //res.status(200).json(response.data);
-      //} catch(error) {
-      //  console.error(error);
-      //  res.status(500).json({ error: 'Something went wrong with the API call' });
-      //}
-      //  
-    //});
     
 })();
