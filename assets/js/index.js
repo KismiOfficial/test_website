d3eb54e7083ff8289c85f8f63f609e3c7e78ceaf
KismiOfficial <kismisolutions@gmail.com>
2025-02-06 14:42:43 -0800

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -65,6 +65,7 @@ import axios from 'axios';
       
         const responseData = await response.json();
         console.log("Response Data: ", responseData);
+        messageField.textContent="Current Member Data: "+responseData.stringify();
       } catch (error) {
         console.error('Error submitting form:', error);
         messageField.textContent = 'An error occurred while submitting the form.';
