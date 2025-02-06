8b5a1b0c956ee803abbc291e76b36de5f8b681d0
KismiOfficial <kismisolutions@gmail.com>
2025-02-05 21:40:46 -0800

    Update to POST and GET

--- api/sendToHubspot.js
+++ api/sendToHubspot.js
@@ -8,19 +8,12 @@ module.exports = async (req, res) => {
   console.log('Req Method: '+req.method);
 
   if (req.method === 'POST') {
-    try {
+        try {
         
       // HubSpot API endpoint and API key
       const url = 'https://api.hubapi.com/crm/v3/objects/contacts';
-      const data = {
-        "properties": {
-          "email": "example@hubspot.com",
-          "firstname": "Jane",
-          "lastname": "Doe",
-          "phone": "(555) 555-5555"
-        }
-      };
-
+      const data = req.body;
+      
       // Make the POST request to HubSpot
       const response = await axios.post(url, data, {
         headers: {
@@ -29,13 +22,41 @@ module.exports = async (req, res) => {
             'Access-Control-Allow-Origin': '*',
         }
       });
-
+      
       // Return the HubSpot response to the client
       res.status(200).json(response.data);
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: 'Something went wrong with the API call' });
     }
+    //try {
+    //    
+    //  // HubSpot API endpoint and API key
+    //  const url = 'https://api.hubapi.com/crm/v3/objects/contacts';
+    //  const data = {
+    //    "properties": {
+    //      "email": "example@hubspot.com",
+    //      "firstname": "Jane",
+    //      "lastname": "Doe",
+    //      "phone": "(555) 555-5555"
+    //    }
+    //  };
+    //  
+    //  // Make the POST request to HubSpot
+    //  const response = await axios.post(url, data, {
+    //    headers: {
+    //        Authorization: `Bearer ${TOKEN}`,
+    //        'Content-Type': 'application/json',
+    //        'Access-Control-Allow-Origin': '*',
+    //    }
+    //  });
+    //  
+    //  // Return the HubSpot response to the client
+    //  res.status(200).json(response.data);
+    //} catch (error) {
+    //  console.error(error);
+    //  res.status(500).json({ error: 'Something went wrong with the API call' });
+    //}
   } else if (req.method === 'GET'){
     
     try{
@@ -54,10 +75,14 @@ module.exports = async (req, res) => {
       res.status(500).json({ error: 'Something went wrong with the API call' });
     }
     
-  } else if(req.method === 'OPTIONS'){
-    res.status(200).json({ message: 'Options' });
+  } else if (req.method === 'OPTIONS'){
+      res.setHeader('Access-Control-Allow-Origin', '*'); // Allow any origin (you may specify a domain)
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS'); // Allowed methods
+      res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allowed headers
+      res.setHeader('Access-Control-Max-Age', '86400'); // Cache the preflight response for 24 hours
+      return res.status(200).end();
   } else {
-    // If the request method isn't POST, return a 405 error
+    // If the request method isn't one of the above, return a 405 error
     res.status(405).json({ error: 'Method Not Allowed' });
   }
 };
--- assets/js/index.js
+++ assets/js/index.js
@@ -3,17 +3,11 @@ import axios from 'axios';
 const TOKEN = process.env.TOKEN;
 
 (function() {
-    console.log('Test');
+    console.log('Start');
     
     const postbutton = document.getElementById('post-btn');
     postbutton.addEventListener('click', async _ => {
-        const data = {
-          email: 'email@example.com',
-          firstname: 'John',
-          lastname: 'Doe',
-          phone: '123-456-7890'
-        };
-
+      
         fetch('https://test-website-seven-bice.vercel.app/api/sendToHubspot', {
           method: 'POST',
           headers: {
@@ -23,7 +17,14 @@ const TOKEN = process.env.TOKEN;
             'Access-Control-Allow-Methods': '*',
             'Access-Control-Allow-Headers': '*',
           },
-          body: JSON.stringify(data)
+          body: {
+            "properties": {
+              "email": "example@hubspot.com",
+              "firstname": "Martin",
+              "lastname": "Smith",
+              "phone": "(555) 555-5555"
+            }
+          }
         })
           .then(response => response.json())
           .then(data => {
@@ -51,9 +52,10 @@ const TOKEN = process.env.TOKEN;
                 },
                 body: {
                     "properties": {
-                        "email": "testuseremail@gmail.com",
-                        "firstname": "John",
-                        "lastname": "Smith"
+                      "email": "example@hubspot.com",
+                      "firstname": "John",
+                      "lastname": "Doe",
+                      "phone": "(555) 555-5555",
                     }
                 }
               },
@@ -70,26 +72,46 @@ const TOKEN = process.env.TOKEN;
     
     const getbutton = document.getElementById('get-btn');
     getbutton.addEventListener('click', async _ => {
-      console.log('Inside Get');
-      try{
-      const response = await axios.get(
-        'https://test-website-seven-bice.vercel.app/api/sendToHubspot',
-        {
+
+        fetch('https://test-website-seven-bice.vercel.app/api/sendToHubspot', {
+          method: 'GET',
           headers: {
             'Content-Type': 'application/json',
             'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Credentials': 'true',
             'Access-Control-Allow-Methods': '*',
             'Access-Control-Allow-Headers': '*',
-          },
-        },
-      );
-      res.status(200).json(response.data);
-      } catch(error) {
-        console.error(error);
-        res.status(500).json({ error: 'Something went wrong with the API call' });
-      }
-        
+          }
+        })
+          .then(response => response.json())
+          .then(data => {
+            console.log('Success:', data);
+          })
+          .catch((error) => {
+            console.error('Error:', error);
+          });
     });
+      
+      //console.log('Inside Get');
+      //try{
+      //const response = await axios.get(
+      //  'https://test-website-seven-bice.vercel.app/api/sendToHubspot',
+      //  {
+      //    headers: {
+      //      'Content-Type': 'application/json',
+      //      'Access-Control-Allow-Origin': '*',
+      //      'Access-Control-Allow-Credentials': 'true',
+      //      'Access-Control-Allow-Methods': '*',
+      //      'Access-Control-Allow-Headers': '*',
+      //    },
+      //  },
+      //);
+      //res.status(200).json(response.data);
+      //} catch(error) {
+      //  console.error(error);
+      //  res.status(500).json({ error: 'Something went wrong with the API call' });
+      //}
+      //  
+    //});
     
 })();
